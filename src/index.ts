2bb5b045d730ac1498f872a108c4149aa082f082
Lem Canady <lem.canady@gmail.com>
2021-12-17 11:18:42 -0800

    initial conmit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { WebSocketServer } from "ws";
+import telnetlib from "telnetlib";
+import Convert from "ansi-to-html";
+
+const convert = new Convert();
+const wss = new WebSocketServer({ port: 8080 }, () => "MUX Bridge connected.");
+
+wss.on("connection", (ws) => {
+  const c = telnetlib.createConnection({
+    host: "uncharted.digibear.io",
+    port: 2860,
+  });
+
+  // xfer data websocket => Telnet
+  ws.on("message", (data) => {
+    if (c.writable) c.write(data + "\r\n");
+  });
+
+  // xfer data Telnet => Websocket
+  c.on("data", (data) => {
+    console.log(convert.toHtml(data.toString()));
+    ws.send(convert.toHtml(data.toString()));
+  });
+
+  ws.on("close", () => c.end());
+  c.on("close", () => ws.close());
+});
