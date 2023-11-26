f7e6ee70289e228480ec6fa88745f54889267955
Lem Canady <lem.canady@gmail.com>
2023-11-25 20:16:40 -0800

    updating to https

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import { WebSocketServer } from "ws";
 import telnetlib from "telnetlib";
 import Convert from "ansi-to-html";
+import https from "https";
+import { config } from "dotenv";
+import { readFileSync } from "fs";
+
+config();
+
+const key = readFileSync(process.env.KEY || "", "utf8");
+const cert = readFileSync(process.env.CERT || "", "utf8");
 
 const convert = new Convert();
-const wss = new WebSocketServer({ port: 8080 }, () => "MUX Bridge connected.");
+
+const httpsServer = https
+  .createServer({ cert, key })
+  .listen(2086, () =>
+    console.log("MUX Bridge HTTPS Server listening on port 2086")
+  );
+
+const wss = new WebSocketServer({ server: httpsServer }, () =>
+  console.log("MUX Bridge connected.")
+);
 
 wss.on("connection", (ws) => {
   const c = telnetlib.createConnection({
-    host: "bridgetown.io",
-    port: 1851,
+    host: "termv.io",
+    port: 2080,
   });
 
   // xfer data websocket => Telnet
